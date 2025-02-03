a9c2a990f49a3117b6a0b1ca13f59fc663d4718d
JoyBoy <144602492+0PrashantYadav0@users.noreply.github.com>
2025-02-03 21:25:32 +0530

    add database configuration and MongoDB connection setup

---
+++ internal/config/database.go
@@ -0,0 +1,47 @@
+package config
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func InitDB() *mongo.Client {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	mongoDB := os.Getenv("MONGO_URI")
+
+	client, err := mongo.NewClient(options.Client().ApplyURI(mongoDB))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	err = client.Connect(ctx)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("successfully connected to  MongoDB server")
+	return client
+}
+
+var Client *mongo.Client = InitDB()
+
+func OpenCollection(client *mongo.Client, collectionName string) *mongo.Collection {
+	var collection *mongo.Collection = client.Database("Restaurant_Management").Collection(collectionName)
+	return collection
+}
