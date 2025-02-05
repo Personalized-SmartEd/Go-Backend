524ebcdf3d48b9b02eed496a45e82bb64671cfdd
JoyBoy <144602492+0PrashantYadav0@users.noreply.github.com>
2025-02-06 00:32:44 +0530

    configured classroom routes for teacher and students

---
+++ internal/routes/classroom_routes.go
@@ -0,0 +1,31 @@
+package routes
+
+import (
+	"backend/internal/controllers"
+	"backend/internal/middleware"
+
+	"github.com/gin-gonic/gin"
+)
+
+func ClassroomRoutesStudent(r *gin.Engine) {
+
+	r.Use(middleware.AuthenticationStudent())
+	r.GET("/classroom/join/:classroom_id/:student_id", controllers.JoinClassroom())
+	r.GET("/classroom/leave/:classroom_id/:student_id", controllers.LeaveClassroom())
+
+}
+
+func ClassroomRoutesTeacher(r *gin.Engine) {
+
+	r.Use(middleware.AuthenticationTeacher())
+	r.GET("/classroom/get/teacher/:teacher_id", controllers.GetClassroomByTeacherID())
+	r.POST("/classroom/create", controllers.CreateClassroom())
+	r.GET("/classroom/delete/:classroom_id", controllers.DeleteClassroom())
+	r.PUT("/classroom/update/:classroom_id", controllers.UpdateClassroom())
+	r.GET("/classroom/get/classroom/:classroom_id", controllers.GetClassroomByID())
+	r.GET("/classroom/get/students/:classroom_id", controllers.GetStudentsByClassroomID())
+	r.GET("/classroom/get/teachers/:classroom_id", controllers.GetTeachersByClassroomID())
+	r.GET("/classroom/get/school/:school_code", controllers.GetClassroomsBySchoolCode())
+	r.GET("/classroom/get/school/:school_code/:teacher_id", controllers.GetClassroomsBySchoolCodeTeacherID())
+
+}
