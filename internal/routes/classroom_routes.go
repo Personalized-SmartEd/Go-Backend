ecd748f9e82ca965a0377276ae241ca7fbad9850
JoyBoy <144602492+0PrashantYadav0@users.noreply.github.com>
2025-02-12 23:16:59 +0530

    refactor: update classroom join logic and restore endpoint to retrieve students by classroom ID

--- internal/routes/classroom_routes.go
+++ internal/routes/classroom_routes.go
@@ -12,8 +12,7 @@ func ClassroomRoutesStudent(r *gin.Engine) {
 	r.Use(middleware.AuthenticationStudent())
 	r.GET("/classroom/join/:classroom_id", controllers.JoinClassroom())
 	r.GET("/classroom/leave/:classroom_id", controllers.LeaveClassroom())
-	r.GET("/classroom/get/students/:classroom_id", controllers.GetStudentsByClassroomID())
-
+	r.GET("/classroom/get/student/:classroom_id", controllers.GetClassroomByID())
 }
 
 func ClassroomRoutesTeacher(r *gin.Engine) {
@@ -23,6 +22,7 @@ func ClassroomRoutesTeacher(r *gin.Engine) {
 	r.POST("/classroom/create/:teacher_id", controllers.CreateClassroom())
 	r.GET("/classroom/delete/:classroom_id", controllers.DeleteClassroom())
 	r.GET("/classroom/get/:classroom_id", controllers.GetClassroomByID())
+	r.GET("/classroom/get/students/:classroom_id", controllers.GetStudentsByClassroomID())
 	r.GET("/classroom/get/teachers/:classroom_id", controllers.GetTeachersByClassroomID())
 	r.GET("/classroom/get/school/:school_code", controllers.GetClassroomsBySchoolCode())
 	r.GET("/classroom/get/school/:school_code/:teacher_id", controllers.GetClassroomsBySchoolCodeTeacherID())
