847d0053d89f5a666dd85aa969511c4604d2793f
JoyBoy <144602492+0PrashantYadav0@users.noreply.github.com>
2025-03-21 20:59:35 +0530

    feat: added new features for summary of pdf and youtube video

--- internal/controllers/quiz_controller.go
+++ internal/controllers/quiz_controller.go
@@ -17,7 +17,7 @@ import (
 
 func PostQuizBot() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		ctx, cancel := context.WithTimeout(context.Background(), 50*time.Second)
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
 		defer cancel()
 
 		var requestBody utils.QuizInput
